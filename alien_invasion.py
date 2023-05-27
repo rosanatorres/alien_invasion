17be2544d558ffca8caf8100515958bd1619cd4b
rosanatorres <rosanacardoso297@hotmail.com>
2023-05-27 15:12:03 +0100

    Insert code and created new file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
-
 import pygame
+from setting import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -8,14 +9,23 @@ class AlienInvasion:
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+
+    
     # Start the main loop for the game.
     while True:
+        
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # Redesenha a tela a cada passagem pelo
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 run_game()
